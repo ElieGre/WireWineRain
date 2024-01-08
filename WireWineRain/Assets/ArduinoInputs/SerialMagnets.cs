d21f990bf0b7fd77eee9c0e5a8f8380d4ea77d01
Vitantonio Greco <1836528+Hurock@users.noreply.github.com>
2024-01-08 16:24:53 +0000

    .NETFramework

---
+++ WireWineRain/Assets/ArduinoInputs/SerialMagnets.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO.Ports;
+using System.Threading;
+using UnityEditor.VersionControl;
+
+public class SerialMagnets : MonoBehaviour
+{
+    Thread IOThread = new Thread(DataThread);
+    public static SerialPort sp;
+    static string incomingMsg = "";
+    public static string outgoingMsg = "";
+
+    private static void DataThread()
+    {
+        sp = new SerialPort("COM3", 9600);
+        sp.Open();
+
+        while(true)
+        {
+            if (outgoingMsg != "")
+            {
+                sp.WriteLine(outgoingMsg);
+                outgoingMsg = "";
+            }
+
+            incomingMsg = sp.ReadExisting();
+            Thread.Sleep(200);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        IOThread.Abort();
+        sp.Close();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        try
+        {
+            IOThread.Start();
+        }
+        catch 
+        {
+            Debug.Log("Start Error");
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        try
+        {
+            if(incomingMsg != "")
+            {
+                Debug.Log(incomingMsg);
+            }
+        }
+        catch
+        {
+
+        }
+    }
+
+    public void SendMessageToArduino(string Message)
+    {
+        //if(Message != "")
+        //{
+        //    sp.WriteLine(Message);
+        //    Message = "";
+           
+        //}
+    }
+}
